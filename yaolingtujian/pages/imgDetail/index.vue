72e0dd7161d468f3b88d5fcd834f17b597bb464a
1095275144@qq.com <614355166@qq.com>
2019-09-07 17:43:48 +0800

    妖灵图鉴

---
+++ yaolingtujian/pages/imgDetail/index.vue
@@ -0,0 +1,646 @@
+<template>
+	<view class="content">
+		<view class="top">
+			<view>妖灵图鉴</view>
+		</view>
+		<view class="detail">
+			<view class="left">
+				<img src="/static/drawable-xhdpi/ILLUSTRATION_Arrow_Left.png" alt="">
+			</view>
+			<view class="detailimg">
+				<img src="http://hy.gwgo.qq.com/sync/pet/512_body/5_XiaoHuanXiong_big.png" alt="">
+			</view>
+			<view class="right">
+				<img src="/static/drawable-xhdpi/ILLUSTRATION_Arrow_Right.png" alt="">
+			</view>
+		</view>
+		<view class="introduction">
+			<view class="data">
+				<view class="datatitle">
+					<view class="segmentation">
+						<view class="leftcircle">
+						</view>
+						<view class="dashed"></view>
+						<view class="rightcircle">
+						</view>
+					</view>
+					<view class="title">
+						妖灵数据<text style="font-size: 25upx; color:#666;">(资质32)</text>
+					</view>
+				</view>
+				<view class="grade">
+					<view class="now">
+						<view class="lowest">当前</view>
+						<view class="jishu">
+							(LV:1)
+						</view>
+					</view>
+					<view class="circle">
+					</view>
+					<view class="line">
+					</view>
+					<view class="highest">
+						最高
+						<view class="jishu">
+							(LV:20)
+						</view>
+					</view>	
+				</view>
+				<view class="parameter">
+					<view class="picture">
+						<view>
+							<img class="img1" src="/static/drawable-xhdpi/ILLUSTRATION_physical_attack_Icon.png" alt="">
+						</view>
+						<view>
+							<img class="img2" src="/static/drawable-xhdpi/ILLUSTRATION_Physical_defense_Icon.png" alt="">
+						</view>
+						<view>
+							<img class="img3" src="/static/drawable-xhdpi/ILLUSTRATION_Spell_attacks_Icon.png" alt="">
+						</view>
+						<view>
+							<img  class="img4" src="/static/drawable-xhdpi/ILLUSTRATION_Spell_defense_Icon.png" alt="">
+						</view>
+						<view>
+							<img  class="img5" src="/static/drawable-xhdpi/ILLUSTRATION_health_point_Icon.png" alt="">
+						</view>
+					</view>
+					<view class="description">
+						<view>
+							物攻
+						</view>
+						<view>
+							物防
+						</view>
+						<view>
+							法攻
+						</view>
+						<view>
+							法防
+						</view>
+						<view>
+							生命
+						</view>
+					</view>
+					<view class="schedule">
+						<view class="wugong">
+							 <progress percent="20" activeColor="#6fdee7" backgroundColor="#4c4c4c" stroke-width="21"/>
+						</view>
+						<view class="wufang">
+							 <progress percent="3" activeColor="#6fdee7" backgroundColor="#4c4c4c" stroke-width="21"/>
+						</view>
+						<view class="fagong">
+							 <progress percent="14" activeColor="#6fdee7" backgroundColor="#4c4c4c" stroke-width="21"/>
+						</view>
+						<view class="fafang">
+							 <progress percent="18" activeColor="#6fdee7" backgroundColor="#4c4c4c" stroke-width="21"/>
+						</view>
+						<view class="life">
+							 <progress percent="23" activeColor="#6fdee7" backgroundColor="#4c4c4c" stroke-width="21"/>
+						</view>
+					</view>
+					<view class="digital">
+						<view class="digital1">
+							246
+						</view>
+						<view class="digital2">
+							285
+						</view>
+						<view class="digital3">
+							246
+						</view>
+						<view class="digital4">
+							300
+						</view>
+						<view class="digital5">
+							1035
+						</view>
+					</view>
+				</view>
+				<view class="specific">
+					<view class="datatitle" style="position: relative; top: 118.05upx">
+						<view class="segmentation">
+							<view class="leftcircle">
+							</view>
+							<view class="dashed"></view>
+							<view class="rightcircle">
+							</view>
+						</view>
+						<view class="retitle">
+							克制关系
+						</view>
+					</view>
+					<view class="relationship">
+						<view class="restraintAttr">
+							<view class="restrain">
+								克制
+							</view>
+							<view class="resimg">
+								<view style="width:55.55upx;">
+									<img style="width:100%;" src="/static/drawable-xhdpi/Property_80px_Shui.png" alt="">
+								</view>
+								<view style="width:55.55upx; margin-left: 11.11upx;">
+									<img style="width:100%;" src="/static/drawable-xhdpi/Property_80px_Yao.png" alt="">
+								</view>
+							</view>
+							<view class="restext" style="margin-top:9.72upx">
+								擂台中输出<text style="color: #fb9605;">1.5</text>倍伤害
+							</view>
+							<view class="restext" style="margin-top:5.55upx">
+								御灵团战、御灵试炼中受到<text style="color: #fb9605;">3</text>倍伤害
+							</view>
+						</view>
+						<view class="beRestraintAttr">
+							<view class="beRestrain">
+								克制
+							</view>
+							<view class="resimg">
+								<view style="width:55.55upx;">
+									<img style="width:100%;" src="/static/drawable-xhdpi/Property_80px_Mu.png" alt="">
+								</view>
+								<view style="width:55.55upx; margin-left: 11.11upx;">
+									<img style="width:100%;" src="/static/drawable-xhdpi/Property_80px_Lei.png" alt="">
+								</view>
+							</view>
+							<view class="restext" style="margin-top:9.72upx">
+								擂台中输出<text style="color: #fb9605;">1.5</text>倍伤害
+							</view>
+							<view class="restext" style="margin-top:5.55upx">
+								御灵团战、御灵试炼中受到<text style="color: #fb9605;">3</text>倍伤害
+							</view>
+						</view>
+					</view>
+					<view class="datatitle" style="position: relative; top:84.72upx">
+						<view class="segmentation">
+							<view class="leftcircle">
+							</view>
+							<view class="dashed"></view>
+							<view class="rightcircle">
+							</view>
+						</view>
+						<view class="retitle">
+							技能详情
+						</view>
+					</view>
+					<view class="skill">
+						<view class="skillDetail">
+							<view class="skillTop">
+								<view class="choupi">
+									臭屁
+								</view>
+								<view class="cdimg">
+									<img sytle="width:100%;" src="/static/drawable-xhdpi/ILLUSTRATION_consume_Icon.png" alt="">
+									<text style="color: #7feded; margin-left: 10.41upx;">2</text>
+								</view>
+								<view class="cd">
+									CD:<text style="color: #7feded">2</text>秒
+								</view>
+							</view>
+							<view class="skillBottom">
+								臭不可闻！！！造成<text style="color: #ff6969">44%</text>的物理攻击力伤害
+							</view>
+						</view>
+						<view class="skillDescription">升级详情</view>
+						<view class="skillGrade">
+							<view class="lv2">
+								<text style="color:#93bad4;font-size: 23.61upx;margin-right:22.91upx">LV2</text>
+								造成<text style="color:#ff6969">49%</text>的物理攻击力伤害
+								<view class="xuxian"></view>
+								<view class="yuan"></view>
+							</view>
+							<view class="lv3">
+								<text style="color:#93bad4;font-size: 23.61upx;margin-right:22.91upx">LV3</text>
+								造成<text style="color:#ff6969">55%</text>的物理攻击力伤害
+								<view class="xuxian"></view>
+								<view class="yuan"></view>
+							</view>
+							<view class="lv4">
+								<text style="color:#93bad4;font-size: 23.61upx;margin-right:22.91upx">LV4</text>
+								造成<text style="color:#ff6969">60%</text>的物理攻击力伤害
+								<view class="xuxian"></view>
+								<view class="yuan"></view>
+							</view>
+							<view class="lv5">
+								<text style="color:#93bad4;font-size: 23.61upx;margin-right:22.91upx">LV5</text>
+								造成<text style="color:#ff6969">66%</text>的物理攻击力伤害
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniSteps from "@/components/uni-steps/uni-steps.vue" 
+	export default {
+		components:{uniSteps},
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		/* align-items: center;
+		justify-content: center; */
+		font-family: "Source Han Sans CN";
+		background-image: url('/static/drawable-xhdpi/ILLUSTRATION_Background.png');
+	}
+	.top {
+		height:75.69upx;
+		width: 100%;
+		font-size: 41.66upx;
+		text-align: center;
+		color: #f2f2f2;
+		background-image: url('/static/drawable-xhdpi/Back.png');
+		background-repeat: no-repeat;
+		background-position-x: 27.08upx;
+		background-position-y: 6.94upx;
+	}
+	.detail {
+		position: relative;
+		width:100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.left {
+		position:absolute;
+		top: 270.13upx;
+		left: 26.38upx;
+		width:28.47upx;
+		height: 47.91upx;
+	}
+	.right {
+		position: absolute;
+		top: 270.13upx;
+		right: 26.38upx;
+		width: 28.47upx;
+		height: 47.91upx;
+	}
+	.introduction {
+		margin-top: 27.77upx;
+	}
+	.datatitle {
+		display: flex;
+		position: relative;
+		align-items: center;
+		justify-content: center;
+	}
+	.title {
+		height: 34.02upx;
+		width: 270.83upx;
+		position: relative;
+		z-index: 1;
+		font-size:27.77upx;
+		text-align: center;
+		color:#fff;
+		background-color: #93bad4;
+		border-radius: 20.83upx;
+	}
+	.segmentation {
+		xposition: relative;
+	}
+	.dashed {
+		position: absolute;
+		left:100upx;
+		border-bottom: dashed 0.69upx #93bad4;
+		width: 550upx;
+		z-index: 1;
+	}
+	.leftcircle {
+		position: absolute;
+		width: 10.41upx;
+		height:10.41upx;
+		border-radius: 1000upx;
+		background-color: #93bad4;
+		left:100upx;
+		top:12.5upx;
+		z-index: 1;
+	}
+	.rightcircle {
+		position: absolute;
+		width: 10.41upx;
+		height:10.41upx;
+		border-radius: 1000upx;
+		background-color: #93bad4;
+		right:100upx;
+		top:12.5upx;
+		z-index: 1;
+	}
+	.grade {
+		display: flex;
+		flex-direction: row;
+		padding: 51.38upx 61.11upx 0px 61.8upx;
+		justify-content: space-between;
+	}
+	.circle {
+		width:38.88upx;
+		height: 38.88upx;
+		border-radius: 69.44upx;
+		background-color: #57b4fe;
+	}
+	.line {
+		width: 431.25upx;
+		height: 1.38upx;
+		background-color: #999;
+		position: relative;
+		top: 19.44upx;
+		left: -13.88upx;
+	}
+	.jishu {
+		font-size: 19.44upx;
+		text-align: center;
+		color: #5aadfc;
+	}
+	.lowest {
+		font-size: 26.38upx;
+		text-align: center;
+		color: #666;
+	}
+	.highest {
+		font-size: 26.38upx;
+		text-align: center;
+		color: #666;
+	}
+	.parameter {
+		display: flex;
+		margin: 55.55upx 47.22upx 0px;
+		width: 659.02upx;
+		flex-direction: row;
+		padding-top: 43.05upx;
+		padding-left: 25upx;
+		background-color: #f2f2e9;
+		color: #666;
+		border-radius: 13.88upx;
+		box-sizing: border-box;
+	}
+	.picture {
+		display: flex;
+		width: 23.61upx;
+		flex-direction: column;
+		padding-top:5.55upx;
+		padding-bottom: 42.36upx;
+		justify-content: space-between;
+	}
+	.picture img {
+		position:relative;
+		width: 23.61upx;
+	}
+	.img1 {
+		top: -6.94upx;
+	}
+	.img2 {
+		top: -2.08upx;
+	}
+	.img4 {
+		top:6.94upx;
+	}
+	.img5 {
+		top: 9.02upx;
+	}
+	.description {
+		font-size: 23.61upx;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		padding-top: 4.86upx;
+		padding-bottom: 42.36upx;
+	}
+	.description view {
+		position: relative;
+		text-align: center;
+		top: 4.86upx;
+		left: 10.41upx;
+	}
+	.schedule {
+		display: flex;
+		flex-direction: column;
+		padding-top: 8.33upx;
+		padding-bottom: 48.61upx;
+		justify-content: space-between;
+		position:relative;
+		left: 43.75upx;
+	}
+	.schedule view {
+		position: relative;
+		width:329.86upx;
+	}
+	.wugong {
+		top: 9.02upx;
+	}
+	.wufang {
+		top: 9.02upx;
+	}
+	.fagong {
+		top: 6.94upx;
+	}
+	.fafang {
+		top: 4.86upx;
+	}
+	.life {
+		top: 3.47upx;
+	}
+	.digital {
+		display: flex;
+		flex-direction: column;
+		padding-bottom: 42.36upx;
+		justify-content: space-between;
+		position:relative;
+		left: 109.02upx;
+		font-size: 29.16upx;
+	}
+	.digital view {
+		position: relative;
+	}
+	.digital1 {
+		top: 3.47upx;
+	}
+	.digital2 {
+		top: 6.94upx;
+	}
+	.digital3 {
+		top: 6.94upx;
+	}
+	.digital4 {
+		top: 6.94upx;
+	}
+	.digital5 {
+		top: 6.94upx;
+	}
+	.specific {
+		position: relative;
+	}
+	.relationship {
+		display: flex;
+		flex-direction: column;
+		font-size: 22.22upx;
+		position:relative;
+		margin: 101.38upx 47.22upx 0upx;
+	}
+	.restraintAttr {
+		width: 659.02upx;
+		height: 227.77upx;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		background-color: #f2f2e9;
+		border-radius: 13.88upx;
+	}
+	.restrain {
+		width:74.3upx;
+		height: 29.86upx;
+		border-radius: 15.27upx;
+		background-color: #2d7bc4;
+		color: #fff;
+		text-align: center;
+		margin-top:23.61upx;
+		margin-left: 33.33upx;
+	}
+	.resimg {
+		display: flex;
+		flex-direction: row;
+		margin:18.75upx auto 0 65.97upx;	
+	}
+	.restext {
+		margin-left: 65.97upx;
+	}
+	.beRestraintAttr {
+		width: 659.02upx;
+		height: 227.77upx;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		background-color: #f2f2e9;
+		border-radius: 13.88upx;
+		margin-top:6.94upx;
+	}
+	.beRestrain {
+		width:95.13upx;
+		height: 29.86upx;
+		border-radius: 15.27upx;
+		background-color: #dc5c47;
+		color: #fff;
+		text-align: center;
+		margin-top:23.61upx;
+		margin-left: 29.16upx;
+	}
+	.retitle {
+		height: 34.02upx;
+		width: 159.72upx;
+		position: relative;
+		z-index: 2;
+		font-size:27.77upx;
+		text-align: center;
+		color:#fff;
+		background-color: #93bad4;
+		border-radius: 20.83upx;
+	}
+	.skill {
+		display: flex;
+		flex-direction: column;
+	}
+	.skillDetail {
+		width: 655.55upx;
+		height: 162.5upx;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		background-color: #f2f2e9;
+		border-radius: 13.88upx;
+		margin: 247.91upx 47.22upx 0upx;
+	}
+	.skillTop {
+		display: flex;
+		flex-direction: row;
+		xjustify-content: space-between;
+		width: 331.25upx;
+	}
+	.choupi {
+		font-size: 29.16upx;
+		color: #666;
+	}
+	.cd {
+		font-size: 23.61upx;
+		color: #666;
+		position:relative;
+		left: 163.19upx;
+		top: 6.94upx;
+	}
+	.cdimg {
+		font-size: 23.61upx;
+		color: #666;
+		position:relative;
+		left: 98.61upx;
+		top: 6.94upx;
+	}
+	.skillBottom {
+		font-size: 18.05upx;
+		color:#666;
+	}
+	.skillDescription {
+		font-size: 29.16upx;
+		color: #666;
+		margin: 23.61upx auto 11.8upx 47.22upx;
+	}
+	.skillGrade {
+		width: 654.86upx;
+		height: 409.02upx;
+		box-sizing: border-box;
+		background-color: #f2f2e9;
+		border-radius: 13.88upx;
+		margin:0 47.22upx 43.05upx;
+		font-size: 25upx;
+		color: #666;
+	}
+	.lv2 {
+		position: relative;
+		top: 27.08upx;
+		left: 33.33upx;
+	}
+	.lv3 {
+		position: relative;
+		top: 96.52upx;
+		left: 33.33upx;
+	}
+	.lv4 {
+		position: relative;
+		top: 165.97upx;
+		left: 33.33upx;
+	}
+	.lv5 {
+		position: relative;
+		top:235.41upx;
+		left: 33.33upx;
+	}
+	.xuxian {
+		position:absolute;
+		height: 65.27upx;
+		border-left: #93bad4 0.69upx dashed;
+		left: 18.75upx;
+	}
+	.yuan {
+		position: absolute;
+		width:6.94upx;
+		height: 6.94upx;
+		border-box: box-sizing;
+		background-color: #93bad4;
+		border-radius: 694.44upx;
+		left: 16.66upx;
+		top: 97.22upx;
+	}
+</style>
---
+++ yaolingtujian/pages/index/index.vue
@@ -0,0 +1,141 @@
+<template>
+	<view class="content">
+		<view class="top">
+			<view class="search">
+				<input type="text"  value="" />
+			</view>
+			<view class="nav">
+				<view>属性</view>
+				<view>图集</view>
+				<view>定位</view>
+				<view>排序</view>
+			</view>
+		</view>
+		<view class="contain">
+			<view class="imgshow" v-for="(item,index) of spriteData">
+					<view class="mainImg">
+						<img style="width:100%;" v-bind:src= "item.bodyImgName">
+					</view>
+					<view class="characteristic" >
+						<img v-if="item.fiveEle[0] ==='金'" src="/static/drawable-xhdpi/Property_40px_Jin.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='木'" src="/static/drawable-xhdpi/Property_40px_Mu.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='水'" src="/static/drawable-xhdpi/Property_40px_Shui.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='火'" src="/static/drawable-xhdpi/Property_40px_Huo.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='土'" src="/static/drawable-xhdpi/Property_40px_Tu.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='无'" src="/static/drawable-xhdpi/Property_40px_Wu.png" mode="">
+						<img v-else-if="item.fiveEle[0] ==='鬼'" src="/static/drawable-xhdpi/Property_40px_Gui.png" mode="">
+					</view>
+					<view class="name">
+						{{item.name}}
+					</view>
+			</view>
+		</view>
+		<view class="footer">
+			<view>正在努力加载中......</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import sprite from '../../yaolingtujian.json'
+		export default {
+			data() {
+				return {
+					sprite: sprite,
+					spriteData: sprite.spriteData 
+				}
+			},
+			onLoad() {
+				this.spriteData.forEach(item => {
+					item.bodyImgName = this.sprite.bodyImgUrl + item.bodyImgName
+				})
+			},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		font-family: "Source Han Sans CN";
+		color:#666;
+	}
+	.top {
+		background-image: url("/static/drawable-xhdpi/妖灵图鉴最上底色图.png");
+	}
+	.search {
+		width:705.55upx;
+		margin:18.75upx 15.97upx 15.27upx;
+		height:55.55upx;
+		border: solid 0.69upx #333333;
+		border-radius: 10.41upx;
+		background-color: #fff;
+	}
+	.nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.nav view {
+		width: 177.77upx;
+		height: 63.88upx;
+		xbackground-image: url("/static/drawable-xhdpi/ILLUSTRATION_Arrow_Under.png");
+		xbackground-position-x: 70%;
+		xbackground-position-y: 50%;
+		xbackground-repeat: no-repeat;
+		font-size: 25upx;
+		line-height: 63.88upx;
+		background-color: #fff;
+		text-align: center;
+		border: solid #666 0.69upx;
+		border-top-left-radius: 13.88upx;
+		border-top-right-radius: 13.88upx;
+	}
+	.contain {
+		display: flex;
+		padding: 17.36upx 27.08upx 18.75upx 27.08upx;
+		flex-direction: row;
+		flex-wrap:wrap;
+		justify-content: space-between;
+		background-color: #f7f7f7;
+	}
+	.imgshow {
+		background-image: url("/static/drawable-xhdpi/ILLUSTRATION_Card_Background.png");
+		background-size: cover;
+		border-radius: 13.88upx;
+		border:solid black 0.69upx;
+		width:214.58upx;
+		height: 292.36upx;
+		margin-top: 14.58upx;
+		position: relative;
+	}
+	.mainImg {
+		width: 100%;
+	}
+	.name {
+		font-size: 25upx;
+		text-align: center;
+		color: #666666;
+		margin:13.88upx;
+	}
+	.footer {
+		text-align: center;
+		font-size: 25upx;
+		margin-bottom: 17.36upx;
+	}
+	.characteristic {
+		position: absolute;
+		xbackground-image: url("/static/drawable-xhdpi/Property_40px_Jin.png");
+		xbackground-size: cover;
+		width:38.88upx;
+		height: 38.88upx;
+		top:11.11upx;
+		left:11.11upx;
+	}
+	.characteristic img {
+		width: 100%;
+	}
+</style>
